dc251eac84a03632332ba065fce58d2759fca01b
Rambharat Patel <80194177+patelrambharat@users.noreply.github.com>
2023-08-15 13:31:29 +0530

    update compile time polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
+
+//compile time polymorphism
+
 public class Main {
+
+    public static class Product{
+
+        public int Product(int x, int y) {
+            return x*y;
+        }
+
+        public int Product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double Product(double x, double y) {
+            return x*y;
+        }
+
+        public  static  void main(String[] args){
+            Product p = new Product();
+            p.Product(3,4);
+            p.Product(3,4,5);
+            p.Product(3.4,4.5);
+        }
+    }
+
 
 }
